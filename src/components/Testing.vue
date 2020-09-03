0c093d5dc426255726366600fa82b2a9649d5836
kaungshine <kaungshine.ks@gmail.com>
2020-09-03 10:40:53 +0630

    Added Testing cli

---
+++ src/components/Testing.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h1>{{msg}}</h1>
+		<p>This is the testing vue</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Testing',
+		props: {
+			msg: String
+		}
+	}
+</script>
+
+<style>
+
+</style>
